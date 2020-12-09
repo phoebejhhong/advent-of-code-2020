4874b27aa048f8d9c006507c29f757c1643106e5
Phoebe Hong <phoebejhhong@gmail.com>
2020-12-09 21:29:41 +1100

    day 4 part 2 done

--- 04-passport-processing.js
+++ 04-passport-processing.js
@@ -23,9 +23,51 @@ const MANEATORY_FIELDS = [
   "pid",
 ];
 
+function isValidYear(year, min, max) {
+  return /^\d{4}$/.test(year) && year >= min && year <= max;
+}
+
+function isValidHeight(value) {
+  if (value.length < 3) {
+    return false;
+  }
+  const unit = value.substring(value.length - 2);
+  const height = value.substring(0, value.length - 2)
+
+  if (unit === "cm") {
+    return height >= 150 && height <= 193;
+  } else if (unit === "in") {
+    return height >= 59 && height <= 76;
+  }
+  return false;
+}
+
+function isValueValid (key, value) {
+  switch (key) {
+    case "byr":
+      return isValidYear(value, 1920, 2002);
+    case "iyr":
+      return isValidYear(value, 2010, 2020);
+    case "eyr":
+      return isValidYear(value, 2020, 2030);
+    case "hgt":
+      return isValidHeight(value);
+    case "hcl":
+      return /^#[abcdef\d]{6}$/.test(value);
+    case "ecl":
+      return ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].indexOf(value) != -1;
+    case "pid":
+    return /^\d{9}$/.test(value);
+    default:
+      return false;
+  }
+}
+
 function isPassportValid(passport) {
   for (i = 0; i < MANEATORY_FIELDS.length; i++) {
-    if (!passport[MANEATORY_FIELDS[i]]) {
+    const field = MANEATORY_FIELDS[i]
+    const value = passport[field];
+    if (!value || !isValueValid(field, value)) {
       return false;
     }
   }
